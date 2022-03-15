53e1f35928df243beb099f8c1c6a60bd82cc90d1
pinankacharya <sujalacharya67@gmail.com>
2022-03-16 02:37:17 +0530

    redux part

---
+++ src/components/redux/reducers/carsReducers.js
@@ -0,0 +1,12 @@
+const initialData = {
+    cars: [],
+};
+
+const carsReducer = (state = initialData, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default carsReducer;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import store from "./components/redux/Store";
+import { provider } from "react-redux";
 ReactDOM.render(<App />, document.getElementById("root"));
