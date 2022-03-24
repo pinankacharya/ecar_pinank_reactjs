b5b3343a253d261c4f8f3eff5119866d4e726ea5
pinankacharya <sujalacharya67@gmail.com>
2022-03-24 13:48:29 +0530

    login changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,55 @@
 import React from "react";
-import "./Login.css";
-import mainLogo from "../shared/1.png";
+import { Row, Col, Form, Input } from "antd";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import "../pages/Login.css";
+import { userLogin } from "../components/redux/actions/userActions";
+// import AOS from "aos";
 
+// import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+// AOS.init();
 function Login() {
+    const dispatch = useDispatch();
+    // const { loading } = useSelector((state) => state.alertsReducer);
+    function onFinish(values) {
+        dispatch(userLogin(values));
+        console.log(values);
+    }
+
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-3 col-md-2" />
-                <div className="col-lg-6 col-md-8 login-box">
-                    <div className="col-lg-12 login-key">
-                        <i className="fa fa-key" aria-hidden="true" />
-                    </div>
-                    <div className="bg">
-                        <img src={mainLogo} />
-                    </div>
-                    {/* <div className="col-lg-12 login-title">Login</div> */}
+        <div className="login">
+            {/* {loading && <Spinner />} */}
+            <Row gutter={16} className="d-flex align-items-center">
+                <Col lg={16} style={{ position: "relative" }}>
+                    <img
+                        className="w-100"
+                        data-aos="slide-left"
+                        data-aos-duration="1500"
+                        src="https://images.unsplash.com/photo-1532268116505-8c59cc37d2e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1164&q=80"
+                    />
+                </Col>
+                <Col lg={8} className="text-left p-5">
+                    <Form layout="vertical" className="login-form p-5" onFinish={onFinish}>
+                        <h1>Login</h1>
+                        <hr />
+                        {/* <Form.Item name="firstName" label="Firstname" rules={[{ required: true }]}>
+                            <Input />
+                        </Form.Item> */}
+                        <Form.Item name="email" label="Email" rules={[{ required: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item name="password" label="Password" rules={[{ required: true }]}>
+                            <Input />
+                        </Form.Item>
 
-                    <div className="col-lg-12 login-form">
-                        <div className="col-lg-12 login-form">
-                            <form>
-                                <div className="form-group">
-                                    <label className="form-control-label">USERNAME</label>
-                                    <input type="text" className="form-control" required />
-                                </div>
-                                <div className="form-group">
-                                    <label className="form-control-label">PASSWORD</label>
-                                    <input type="password" className="form-control" required />
-                                </div>
-                                <div className="col-lg-12 loginbttm">
-                                    <div className="col-lg-6 login-btm login-text">{/* Error Message */}</div>
-                                    <div className="col-lg-6 login-btm login-button">
-                                        <button type="submit" className="btn btn-outline-primary">
-                                            LOGIN
-                                        </button>
-                                        <br />
-                                        <br />
-                                        <Link to="/Register">Signup</Link>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                        <button className="btn1 mt-2 mb-3">Login</button>
+                        <br />
 
-                    <div className="col-lg-3 col-md-2" />
-                </div>
-            </div>
+                        <Link to="/Register">Click Here to Register</Link>
+                    </Form>
+                </Col>
+            </Row>
         </div>
     );
 }
