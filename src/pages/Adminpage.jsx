e195ea8030f2cabc80c62f1a0b294a67a276e395
pinankacharya <sujalacharya67@gmail.com>
2022-05-10 01:06:30 +0530

    admin

--- src/pages/Adminpage.jsx
+++ src/pages/Adminpage.jsx
@@ -1,73 +1,207 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Button, Modal, Input } from "react-bootstrap";
 
-import Axios from "axios";
 // import Navbar from "../components/Admin/Navbar";
 
-export const Adminpage = () => {
+function AdminPage() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">Basic Table</h4>
-                <p class="card-description">
-                    Add class <code>.table</code>
-                </p>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Profile</th>
-                                <th>VatNo.</th>
-                                <th>Created</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Jacob</td>
-                                <td>53275531</td>
-                                <td>12 May 2017</td>
-                                <td>
-                                    <label class="badge badge-danger">Pending</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Messsy</td>
-                                <td>53275532</td>
-                                <td>15 May 2017</td>
-                                <td>
-                                    <label class="badge badge-warning">In progress</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>John</td>
-                                <td>53275533</td>
-                                <td>14 May 2017</td>
-                                <td>
-                                    <label class="badge badge-info">Fixed</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Peter</td>
-                                <td>53275534</td>
-                                <td>16 May 2017</td>
-                                <td>
-                                    <label class="badge badge-success">Completed</label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Dave</td>
-                                <td>53275535</td>
-                                <td>20 May 2017</td>
-                                <td>
-                                    <label class="badge badge-warning">In progress</label>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <div class="container ">
+            <div className="crud shadow-lg p-3 mb-5 mt-5 bg-body rounded">
+                <div class="row ">
+                    <div class="col-sm-3 mt-5 mb-4 text-gred">
+                        <div className="search">
+                            <form class="form-inline">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search Student" aria-label="Search" />
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 offset-sm-2 mt-5 mb-4 text-gred" style={{ color: "green" }}>
+                        <h2>
+                            <b>Artists</b>
+                        </h2>
+                    </div>
+                    <div class="col-sm-3 offset-sm-1  mt-5 mb-4 text-gred">
+                        <Button variant="primary" onClick={handleShow}>
+                            Add New Artists
+                        </Button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="table-responsive ">
+                        <table class="table table-striped table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name </th>
+                                    <th>Address</th>
+                                    <th>City </th>
+                                    <th>Country </th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Rual Octo</td>
+                                    <td>Deban Steet</td>
+                                    <td>Newyork</td>
+                                    <td>USA</td>
+                                    <td>
+                                        <a href="#" class="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}>
+                                            <i class="material-icons">&#xE417;</i>
+                                        </a>
+                                        <a href="#" class="edit" title="Edit" data-toggle="tooltip">
+                                            <i class="material-icons">&#xE254;</i>
+                                        </a>
+                                        <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{ color: "red" }}>
+                                            <i class="material-icons">&#xE872;</i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Demark</td>
+                                    <td>City Road.13</td>
+                                    <td>Dubai</td>
+                                    <td>UAE</td>
+                                    <td>
+                                        <a href="#" class="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}>
+                                            <i class="material-icons">&#xE417;</i>
+                                        </a>
+                                        <a href="#" class="edit" title="Edit" data-toggle="tooltip">
+                                            <i class="material-icons">&#xE254;</i>
+                                        </a>
+                                        <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{ color: "red" }}>
+                                            <i class="material-icons">&#xE872;</i>
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>3</td>
+                                    <td>Richa Deba</td>
+                                    <td>Ocol Str. 57</td>
+                                    <td>Berlin</td>
+                                    <td>Germany</td>
+                                    <td>
+                                        <a href="#" class="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}>
+                                            <i class="material-icons">&#xE417;</i>
+                                        </a>
+                                        <a href="#" class="edit" title="Edit" data-toggle="tooltip">
+                                            <i class="material-icons">&#xE254;</i>
+                                        </a>
+                                        <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{ color: "red" }}>
+                                            <i class="material-icons">&#xE872;</i>
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>4</td>
+                                    <td>James Cott</td>
+                                    <td>Berut Road</td>
+                                    <td>Paris</td>
+                                    <td>France</td>
+                                    <td>
+                                        <a href="#" class="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}>
+                                            <i class="material-icons">&#xE417;</i>
+                                        </a>
+                                        <a href="#" class="edit" title="Edit" data-toggle="tooltip">
+                                            <i class="material-icons">&#xE254;</i>
+                                        </a>
+                                        <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{ color: "red" }}>
+                                            <i class="material-icons">&#xE872;</i>
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>5</td>
+                                    <td>Dheraj</td>
+                                    <td>Bulf Str. 57</td>
+                                    <td>Delhi</td>
+                                    <td>India</td>
+                                    <td>
+                                        <a href="#" class="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}>
+                                            <i class="material-icons">&#xE417;</i>
+                                        </a>
+                                        <a href="#" class="edit" title="Edit" data-toggle="tooltip">
+                                            <i class="material-icons">&#xE254;</i>
+                                        </a>
+                                        <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{ color: "red" }}>
+                                            <i class="material-icons">&#xE872;</i>
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>6</td>
+                                    <td>Maria James</td>
+                                    <td>Obere Str. 57</td>
+                                    <td>Tokyo</td>
+                                    <td>Japan</td>
+                                    <td>
+                                        <a href="#" class="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}>
+                                            <i class="material-icons">&#xE417;</i>
+                                        </a>
+                                        <a href="#" class="edit" title="Edit" data-toggle="tooltip">
+                                            <i class="material-icons">&#xE254;</i>
+                                        </a>
+                                        <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{ color: "red" }}>
+                                            <i class="material-icons">&#xE872;</i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                {/* <!--- Model Box ---> */}
+                <div className="model_box">
+                    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Add Record</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <form>
+                                <div class="form-group">
+                                    <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" />
+                                </div>
+                                <div class="form-group mt-3">
+                                    <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Biography" />
+                                </div>
+                                <div class="form-group mt-3">
+                                    <input type="role" class="form-control" id="exampleInputRole" aria-describedby="RoleHelp" placeholder="Enter Role" />
+                                </div>
+                                <div class="form-group mt-3">
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Country" />
+                                </div>
+
+                                <button type="submit" class="btn btn-success mt-4">
+                                    Add Record
+                                </button>
+                            </form>
+                        </Modal.Body>
+
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+
+                    {/* Model Box Finsihs */}
                 </div>
             </div>
         </div>
     );
-};
+}
 
-export default Adminpage;
+export default AdminPage;
